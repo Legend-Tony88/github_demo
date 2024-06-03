bbf4d2491ff1c2c884030a7d439b20bd4f0c4e3e
Tanmay Kandpal <tanmaykandpal88@gmail.com>
2024-06-03 15:57:03 +0530

    Changes done by sundar

--- app/src/main/java/com/example/github_tutorial_merge/MainActivity.java
+++ app/src/main/java/com/example/github_tutorial_merge/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // One change I am doing
+        // Sundar has  made some changes
 
     }
 
